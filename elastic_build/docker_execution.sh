78293857e664946cc5eeef96f89ed5bf11e4fc99
cfalcondair <cfalcondair@gmail.com>
2017-06-03 18:01:31 +0200

    Dockerise the elasticsearch component

---
+++ elastic_build/docker_execution.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+# Build custom docker image
+docker build --tag=elasticsearch-custom .
+
+# The vm_max_map_count kernel setting needs to be set to at least 262144 for use
+sudo sysctl -w vm.max_map_count=262144
+
+docker-compose up
+
