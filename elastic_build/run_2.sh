36c4c9ee72a1a8ef569022594fb824d6915c0aad
cfalcondair <cfalcondair@gmail.com>
2017-06-05 11:50:13 +0200

    Start elasticsearch in the background

--- elastic_build/run_2.sh
+++ elastic_build/run_2.sh
@@ -6,5 +6,5 @@ docker build --tag=elasticsearch-custom .
 # The vm_max_map_count kernel setting needs to be set to at least 262144 for use
 sudo sysctl -w vm.max_map_count=262144
 
-docker-compose up
+docker-compose up -d
 
