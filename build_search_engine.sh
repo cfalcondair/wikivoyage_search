10f765621388a133e0ceee7024ca21f6817a2084
cfalcondair <cfalcondair@gmail.com>
2017-06-06 10:58:00 +0200

    Use single docker-compose for elasticsearch build

--- build_search_engine.sh
+++ build_search_engine.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/sh
 
-source run_0.sh
-cd xml_parse && source run_1.sh
-cd ../elastic_build && source run_2.sh && source run_3.sh
-cd ../upload_data && source run_4.sh
-cd ..
+sudo sysctl -w vm.max_map_count=262144
 
+docker-compose up --build
